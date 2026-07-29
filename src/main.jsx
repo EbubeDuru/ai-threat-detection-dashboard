755467c4e5647937db45066577606ef6161bf08c
EbubeDuru <duruebube00@gmail.com>
2026-07-29 02:34:20 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ThreatDetectionDashboard from "./ThreatDetectionDashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThreatDetectionDashboard />
+  </React.StrictMode>
+);
